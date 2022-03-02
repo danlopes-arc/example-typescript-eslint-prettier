cb60c7cd36ce04a70dfe9fedcedf2cd5c2eec2ce
Daniel <danlpsarcanjo@gmail.com>
2022-03-01 23:52:29 -0500

    Add React config

---
+++ .eslintrc.react.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['warn', {
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'endOfLine': 'auto',
+      'printWidth': 100
+    }],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-types': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  }
+}
